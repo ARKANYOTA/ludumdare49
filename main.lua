e74ba6b9891be67d4b8ba16feaa322cb6b267682
Notgoyome <tranguillaume16@gmail.com>
2021-10-02 14:26:40 +0200

    interface des collisions

--- main.lua
+++ main.lua
@@ -1,7 +1,18 @@
 #!/usr/bin/env -S love ./
 -- Permet de l'executer avec ./main.lua
 -- vim: fdm=marker
+
+--METTEZ OU IL FAUT LE METTRE SVP
 require "scripts/entities"
+function see_collision(x,y,w,h)
+	love.graphics.rectangle("fill",x,y,4,h) -- vertical left
+	love.graphics.rectangle("fill",x+w,y,4,h) -- vert right
+	love.graphics.rectangle("fill",x,y,w,4)
+	love.graphics.rectangle("fill",x,y+w,w,4)
+	return 4
+end
+
+
 -- EVENTS{{{1
 function love.load() -- LOAD {{{2
 	player_create()
@@ -48,11 +59,15 @@ function love.draw() -- DRAWING {{{2
 	if menu == 'ingame' then
 		--love.graphics.rectangle("fill",600, 100,100,20,40,1)
 		love.graphics.draw(p.sprite,p.x,p.y,0,p.scale_x,scale_y) --joueur
-		love.graphics.draw(b.sprite,b.x,b.y,0,0.2,0.2)
+		love.graphics.draw(b.sprite,b.x,b.y,0,b.scale_x,b.scale_y)
+
 	end
 	if debug then
+		see_collision(b.x,b.y,b.w,b.h)
+		see_collision(p.x,p.y,p.w,p.h)
 		love.graphics.print(string.format("%.3f",p.x).."/"..string.format("%.3f",p.y), p.x, p.y-50, 0,2,2) -- coordonnées player
-		love.graphics.print(string.format("%.3f",b.x).."/"..string.format("%.3f",b.y), b.x+50, b.y, 0,2,2) -- coordonnées bomb
+		--love.graphics.print(string.format("%.3f",b.x).."/"..string.format("%.3f",b.y), b.x+50, b.y, 0,2,2) -- coordonnées bomb
+		love.graphics.print(b.w, b.x+50, b.y, 0,2,2) -- coordonnées bomb
 		--love.graphics.print(coll,16,16)
 		love.graphics.print(string.format("%.3f",global_timer), 0, 50, 0,2,2) -- arrondi a 3 décimale apres ,
 	end
