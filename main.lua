6eb1e385ec88db1f15239bf84694c232d4362d63
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2021-10-02 11:59:18 +0200

    Menu

--- main.lua
+++ main.lua
@@ -3,36 +3,46 @@
 -- vim: fdm=marker
 
 -- FUNCTIONS {{{1
-function is_odd(n) -- {{{1
-	return n%100==1
-end
+-- is_odd, is_even {{{2
+function is_odd(n) return n%2==1 end
+function is_even(n) return n%2==0 end
 
-function is_even(n)
-	return n%2==0
-end
 -- EVENTS{{{1
 function love.load() -- LOAD {{{2
+	menu = 'ingame'
+	timer = 0
 	x, y, w, h = 20, 20, 60, 20
+	debug = false
 end
 
 function love.update(dt) -- UPDATE {{{2
+	timer = timer + dt
 	w = w + 1
 	h = h + 1
 end
 
 function love.keypressed(key, scancode, isrepeat) -- KEYPRESSED {{{2
+	-- always {{{3
 	if key == "escape" or (love.keyboard.isDown("lctrl") and key == "c") then
 		love.event.quit()
 	end
-	if key == "f5" then
-		love.event.quit("restart")
+	if key == "f5" then love.event.quit("restart") end
+	if key == "f3" then debug = not debug end
+	if menu == "ingame" then -- ingame {{{3
+		
 	end
-	print(key)
+	if menu == "menu" then -- menu {{{3
+
+	end
 end
 
 function love.draw() -- DRAWING {{{2
-	love.graphics.print("Hello World!", 400, 300)
-	love.graphics.rectangle("fill",780,600, 100,100,20,40,1)
-	love.graphics.print(tostring(is_odd(w)), 0, 0)
+	if menu == 'ingame' then
+		love.graphics.print("Hello World!", 400, 300)
+		love.graphics.rectangle("fill",600, 100,100,20,40,1)
+	end
+	if debug then
+		love.graphics.print(tostring(timer), 40, 0)
+	end
 end
 
