41cc564b1582a022942411e314307493c84ac171
Theobosse <60153892+Theobosse@users.noreply.github.com>
2021-10-02 12:11:53 +0200

    modifs cool

--- main.lua
+++ main.lua
@@ -4,8 +4,8 @@
 
 -- FUNCTIONS {{{1
 -- is_odd, is_even {{{2
-function is_odd(n) return n%2==1 end
-function is_even(n) return n%2==0 end
+function is_odd(n) return n % 2 == 1 end
+function is_even(n) return n % 2 == 0 end
 
 -- EVENTS{{{1
 function love.load() -- LOAD {{{2
@@ -26,8 +26,12 @@ function love.keypressed(key, scancode, isrepeat) -- KEYPRESSED {{{2
 	if key == "escape" or (love.keyboard.isDown("lctrl") and key == "c") then
 		love.event.quit()
 	end
-	if key == "f5" then love.event.quit("restart") end
-	if key == "f3" then debug = not debug end
+	if key == "f5" then 
+		love.event.quit("restart") 
+	end
+	if key == "f3" then 
+		debug = not debug 
+	end
 	if menu == "ingame" then -- ingame {{{3
 		
 	end
@@ -38,7 +42,7 @@ end
 
 function love.draw() -- DRAWING {{{2
 	if menu == 'ingame' then
-		love.graphics.print("Hello World!", 400, 300)
+		love.graphics.print("Hello World! (THEOBOSSE IS THE BEST BTW)", 400, 300)
 		love.graphics.rectangle("fill",600, 100,100,20,40,1)
 	end
 	if debug then
