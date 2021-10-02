8c4ac1d2eed01e0e5c45f3e0a74a19b4a532ff1f
Notgoyome <tranguillaume16@gmail.com>
2021-10-02 13:33:46 +0200

    test collision

--- main.lua
+++ main.lua
@@ -9,22 +9,28 @@ function player_create() -- {{{3
 		x=200,
 		y=200,
 		sprite = love.graphics.newImage("assets/player01.png"),
-		speed = 8,
+		speed = 1,
 		scale_x = 1,
 		scale_y = 1
 		}
 end
-
-function bombe_create()
+-- Bombe
+function bomb_create()
     b = {
-	x=0,
-	y=0,
+	x=100,
+	y=100,
 	sprite = love.graphics.newImage("assets/bomb.png")
 
     }
 end
 
-
+function collision(x1,y1,x2,y2)
+	if x1 == x2 and y1 == y2 then
+		return 8
+	else
+		return 2
+	end
+end
 
 function player_movement() --{{{3
 	if love.keyboard.isDown("q") or love.keyboard.isDown("left") then 
@@ -50,7 +56,7 @@ end
 -- EVENTS{{{1
 function love.load() -- LOAD {{{2
 	player_create()
-	bombe_create()
+	bomb_create()
 	menu = 'ingame'
 	ingame_timer = 0
 	global_timer = 0
@@ -63,6 +69,7 @@ function love.update(dt) -- UPDATE {{{2
 	if menu == 'ingame' then
 		global_timer = global_timer + 1
 		player_movement()
+		coll = collision(p.x,p.y,b.x,b.y)
 	end
 end
 
@@ -83,7 +90,9 @@ end
 
 function love.draw() -- DRAWING {{{2
 	if menu == 'ingame' then
-		love.graphics.print(p.x.."/"..p.y, 0, 0, 0,2,2)
+		love.graphics.print(p.x.."/"..p.y, p.x, p.y-50, 0,2,2) -- coordonnées player
+		love.graphics.print(b.x.."/"..b.y, b.x+50, b.y, 0,2,2) -- coordonnées bomb
+		love.graphics.print(coll, 0, 0, 0,2,2)
 		love.graphics.print(string.format("%.3f",timer), 0, 50, 0,2,2) -- arrondi a 3 décimale apres ,
 		love.graphics.rectangle("fill",600, 100,100,20,40,1)
 		
