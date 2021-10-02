672fdf57310828b7c232b22d376823ca4feb988c
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2021-10-02 20:11:27 +0200

    Fix

--- scripts/entities.lua
+++ scripts/entities.lua
@@ -184,8 +184,8 @@ function update_bomb(dt)
 end
 
 function draw_bomb()
-    love.graphics.draw(b.sprite, b.x - b.w/2, b.y - b.h/2, 0, b.scale_x, b.scale_y)
-    --love.graphics.draw(b.sprite, b.x - b.w/b.sprite:getWidth(), b.y - b.h/b.sprite:getHeight(), 0, b.scale_x, b.scale_y)
+    --love.graphics.draw(b.sprite, b.x - b.w/2, b.y - b.h/2, 0, b.scale_x, b.scale_y)
+    love.graphics.draw(b.sprite, b.x - b.w/b.sprite:getWidth(), b.y - b.h/b.sprite:getHeight(), 0, b.scale_x, b.scale_y)
 end
 
 function player_update()
