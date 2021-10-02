897087d613006732d2d88b1f7c62b1e8135be09c
Notgoyome <tranguillaume16@gmail.com>
2021-10-02 12:20:51 +0200

    déplacement joueur, creation folder sprite

--- main.lua
+++ main.lua
@@ -13,9 +13,11 @@ end
 -- EVENTS{{{1
 function love.load() -- LOAD {{{2
 	x, y, w, h = 20, 20, 60, 20
+	player_create()
 end
 
 function love.update(dt) -- UPDATE {{{2
+	player_movement()
 	w = w + 1
 	h = h + 1
 end
@@ -30,9 +32,45 @@ function love.keypressed(key, scancode, isrepeat) -- KEYPRESSED {{{2
 	print(key)
 end
 
+-----------------
+--Player function 
+-----------------
+
+
+
+
+
 function love.draw() -- DRAWING {{{2
 	love.graphics.print("Hello World!", 400, 300)
-	love.graphics.rectangle("fill",780,600, 100,100,20,40,1)
+	love.graphics.rectangle("fill",750,299, 100,100,20,40,1)
 	love.graphics.print(tostring(is_odd(w)), 0, 0)
+	love.graphics.draw(p.sprite,p.x,p.y,0,0.5,0.5)
 end
 
+
+function player_create()
+	p = {
+		x=0,
+		y=0,
+		sprite = love.graphics.newImage("assets/player.sprite/player01.png"),
+		speed = 8
+		}
+end
+
+function player_movement()
+	if love.keyboard.isDown("left") then 
+		p.x = p.x - p.speed
+	end
+
+	if love.keyboard.isDown("right") then 
+		p.x = p.x + p.speed
+	end
+
+	if love.keyboard.isDown("up") then 
+		p.y = p.y - p.speed
+	end
+
+	if love.keyboard.isDown("down") then
+		 p.y = p.y + p.speed
+	end
+end
