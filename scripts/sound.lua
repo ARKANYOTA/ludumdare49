cfc17f6930d5005a8c00b55864e9898b44c21441
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2021-10-03 16:02:30 +0100

    Metal sounds

--- scripts/sound.lua
+++ scripts/sound.lua
@@ -1,2 +1,2 @@
 snd_woosh = love.audio.newSource("assets/sound/woosh.mp3", "static") -- https://freesound.org/people/lesaucisson/sounds/585257/
-snd_metalbar = love.audio.newSource("assets/sound/metalbar.aiff", "static") -- https://freesound.org/people/jorickhoofd/sounds/160045/
+snd_metalbar = love.audio.newSource("assets/sound/metalbar2.mp3", "static") -- https://freesound.org/people/jorickhoofd/sounds/160045/
