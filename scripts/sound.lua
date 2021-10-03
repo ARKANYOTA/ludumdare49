c53b5dbbcf859c40e3e4ab05fc31e2d8c0c44d59
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2021-10-03 22:12:54 +0100

    Added Enemy Damage sfx

--- scripts/sound.lua
+++ scripts/sound.lua
@@ -8,6 +8,7 @@ snd_bombbounce = love.audio.newSource("assets/sound/bomb_bounce.mp3", "static") 
 -- metal_bar : https://freesound.org/people/jorickhoofd/sounds/160045/
 snd_bombbeep = love.audio.newSource("assets/sound/bomb_beep.mp3", "static") --https://freesound.org/people/SpliceSound/sounds/369880/
 snd_bombboom = love.audio.newSource("assets/sound/explosion.mp3", "static") --https://freesound.org/people/Iwiploppenisse/sounds/156031/
+snd_enemydamage = love.audio.newSource("assets/sound/enemy_damage.wav", "static") --https://freesound.org/people/Deathscyp/sounds/404109/
 
 music_calm = love.audio.newSource("assets/sound/music_calm.wav", "stream")
 music_tense = love.audio.newSource("assets/sound/music_tense.wav", "stream")
